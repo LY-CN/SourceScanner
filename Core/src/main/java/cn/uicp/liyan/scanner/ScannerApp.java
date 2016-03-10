36b60ce30b4bf84d6be6e9ac451a93dd120b9c04
LiYan <shuangyuly@126.com>
2016-03-10 11:54:17 +0800

    初始化项目

---
+++ Core/src/main/java/cn/uicp/liyan/scanner/ScannerApp.java
@@ -0,0 +1,17 @@
+package cn.uicp.liyan.scanner;
+
+/**
+ * @author LiYan
+ * @version 1.0 2016/3/10
+ */
+public class ScannerApp {
+
+    /**
+     * 程序入口函数
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
